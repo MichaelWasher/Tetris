01efa8fd9baab94069252533eed6bff384f5b584
Michael Washer <michael.washer@icloud.com>
2020-06-15 20:59:23 +1200

    Add Class Skeletons

--- scripts/main-game.js
+++ scripts/main-game.js
@@ -1,3 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {
+function Point(x,y){
+    this.x = x;
+    this.y = y;
+}
 
+class TetrisPiece{
+
+    constructor() {
+        this.currentPosition = new Point(0,0);
+        this.colour = "blue";
+        this.rotations = this.getRotations();
+    }
+    draw(){
+
+    }
+    getRotations(){
+
+    }
+}
+class TetrisLPiece extends TetrisPiece{
+    constructor(){
+        super();
+    }
+}
+
+
+class GameLoop{
+
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const width = 10;
+    const height = 20;
+    //Find Squares and Grid
+    const gameGrid = document.querySelector(".main-grid");
+    let squares = document.querySelectorAll(".main-grid div");
+    console.log(`Found Grid ${squares.length} squares in the grid.`)
+
+    //
+    
 })
